9c837ae4ce003a83ccad528f2169dae0403be436
rladbrua0207 <rladbrua0207@gmail.com>
2022-01-09 16:52:48 +0900

    #5.8 Form Errors

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -26,19 +26,53 @@ import { useForm } from "react-hook-form";
   );
 } */
 
+//필수항목이 아니라면 ? 분혀주기
+interface IForm {
+  email: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  password1: string;
+}
+
 function ToDoList() {
-  const { register, handleSubmit, formState } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
 
   //react-hook-form이 모든 validation을 다 마쳤을 때만 호출
   const onValid = (data: any) => {
     console.log(data);
   };
 
-  console.log(formState.errors);
+  console.log(errors);
   return (
     <div>
-      <form onSubmit={handleSubmit(onValid)}>
-        <input {...register("email", { required: true })} placeholder="email" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", {
+            required: {
+              message: "email is required",
+              value: true,
+            },
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          placeholder="email"
+        />
+        <span>{errors?.email?.message}</span>
         <input
           {...register("username", { required: true })}
           placeholder="username"
@@ -58,6 +92,8 @@ function ToDoList() {
           })}
           placeholder="password"
         />
+        {/* required: "message", minLength:{value:? message:"message"} 
+        error객체에 message 전달*/}
         <input
           {...register("password1", {
             required: "Password is required",
